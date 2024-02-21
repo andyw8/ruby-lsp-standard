801873b9b18072f2758225f67ea4e5c8e3235194
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-02-21 08:34:50 -0500

    Initial commit

---
+++ lib/ruby/lsp/standard.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "standard/version"
+
+module Ruby
+  module Lsp
+    module Standard
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
