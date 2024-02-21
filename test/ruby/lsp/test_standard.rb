33874c4cc1457725cc0a4e55506d48b90eaf0fda
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-02-21 08:40:48 -0500

    Remove example test

--- test/ruby/lsp/test_standard.rb
+++ test/ruby/lsp/test_standard.rb
@@ -6,8 +6,4 @@ class Ruby::Lsp::TestStandard < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Ruby::Lsp::Standard::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
